e34a6fe337d3ceea61ce01082353beba8bbc9761
mtumilowicz <michal.tumilowicz01@gmail.com>
2019-01-09 23:19:42 +0100

    Writer syntax apply -> ()

--- src/main/scala/Writer.scala
+++ src/main/scala/Writer.scala
@@ -3,5 +3,5 @@
   */
 class Writer[X](val result: X, val log: String) {
   def map[Y](f: Function[X, Y]): Writer[Y] = 
-    KleisliWriterCategory.compose[Writer[X], X, Y](identity, KleisliWriterCategory.identity.compose(f)).apply(this)
+    KleisliWriterCategory.compose[Writer[X], X, Y](identity, KleisliWriterCategory.identity.compose(f))(this)
 }
