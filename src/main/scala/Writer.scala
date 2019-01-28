3ab76a7f8073d8d38a266552035b11d8f3b1bf55
mtumilowicz <michal.tumilowicz01@gmail.com>
2019-01-28 20:21:01 +0100

    formatting

--- src/main/scala/Writer.scala
+++ src/main/scala/Writer.scala
@@ -2,6 +2,6 @@
   * Created by mtumilowicz on 2019-01-09.
   */
 class Writer[X](val result: X, val log: String) {
-  def map[Y](f: Function[X, Y]): Writer[Y] = 
+  def map[Y](f: Function[X, Y]): Writer[Y] =
     KleisliWriterCategory.compose[Writer[X], X, Y](identity, KleisliWriterCategory.identity.compose(f))(this)
 }
--- src/test/scala/WriterTest.scala
+++ src/test/scala/WriterTest.scala
@@ -3,14 +3,14 @@ import org.scalatest.{FunSuite, Matchers}
 /**
   * Created by mtumilowicz on 2019-01-16.
   */
-class WriterTest  extends FunSuite with Matchers  {
+class WriterTest extends FunSuite with Matchers {
 
   test("testMap") {
     val writer = new Writer[Int](0, "")
     val mapped = writer.map(_ + 1)
-    
-    mapped.result should be (1)
-    mapped.log should be ("")
+
+    mapped.result should be(1)
+    mapped.log should be("")
   }
 
 }
